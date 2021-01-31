2b009f7e46d9a7e36802e843ef62369aa124e125
Agnieszka Brown <abrown2012@icloud.com>
2021-01-30 21:14:49 -0500

    commit tree e261c815ee107da05fd7d6c66ff5a0b3e6f17859

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,49 @@
+require_relative '../../config/environment'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do 
+    erb :new
+  end 
+
+  get '/recipes' do 
+    @recipe = Recipe.all 
+    erb :index
+  end 
+
+  get '/recipes/:id/edit' do 
+    find_recipe
+    erb :edit 
+  end 
+
+  post '/recipes' do 
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{Recipe.last.id}"
+  end
+
+  patch '/recipes/:id' do 
+    find_recipe
+    @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end 
+
+  get '/recipes/:id' do 
+    find_recipe
+    erb :show 
+  end 
+
+  delete '/recipes/:id' do 
+    find_recipe 
+    @recipe.destroy 
+    redirect "/recipes"
+  end 
+
+  def find_recipe
+    @recipe = Recipe.find(params["id"])
+  end
 
 end
